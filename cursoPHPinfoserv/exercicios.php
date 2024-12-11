36291111d09d44eb237b9587dced7a5233988cb1
Gilson Mariani Sartori <gilsonsartori80@gmail.com>
2024-12-10 21:48:12 -0300

    aula_4

---
+++ cursoPHPinfoserv/exercicios.php
@@ -0,0 +1,22 @@
+
+<?php 
+
+// Exercicio 1:
+// Atribuir um valor para uma variavel chamada numero evamos informar ao usuario se este numero é par ou impar.
+
+$numero = 7;
+
+if($numero % 2 == 0) {
+    echo "O número " . $numero . " informado é par.";
+} else {
+    echo "O número " . $numero . " informado é impar.";
+}
+
+// Exercicio 2:
+// Dado um numero informado pelo usuario e se ele é um numero primo
+
+
+
+
+
+?>
