538043de1d976f1f2e3906c199907e1a6db67e57
SartoriGilson <gilsonsartori80@gmail.com>
2024-12-17 21:54:39 -0300

    aula_4

--- cursoPHPinfoserv/Calculadora.php
+++ cursoPHPinfoserv/Calculadora.php
@@ -37,6 +37,41 @@ else {
 
 
 
+$comp_1 = 1;
+$comp_2 = 2;
+$comp_3 = 7;
+
+// as soma de dois segmentos de triangulo tem que ser maior que o terceiro segmento.
+if ($comp_1 < ($comp_2 + $comp_3) && $comp_2 < ($comp_1 + $comp_3) && $comp_3 < ($comp_1 + $comp_2)) { // formula para calcular se é possivel ser um triangulo
+    if ($comp_1 == $comp_2 && $comp_2 == $comp_3) {
+        echo "<br> Com os valores informados temos um triângulo EQUILÁTERO.";
+    } 
+    elseif ($comp_1 == $comp_2 || $comp_1 == $comp_3 || $comp_2 == $comp_3) {
+        echo " <br> Com os valores informados temos um triângulo ISÓSCELES.";
+    } 
+    else {
+        echo "<br> Com os valores informados temos um triângulo ESCALENO.";
+    }
+} 
+else {
+    echo "<br> As medidas fornecidas não podem formar um triângulo.";
+}
+
+
+/** 
+ * criar um algoritimo que simula o funcionamento do calendario.
+ * variaveis: $data = '01/01/2025'
+ * validar a data e mes e ser ano foe bixesto
+ * Dica: if else,
+ * */ 
+
+ $data = '01/01/2025';
+
+ $dia = substr($data, 0, 2);
+ $mes = substr($data, 3, 2);
+ $ano = substr($data, 6, 2);
+
+
 ?>
 
 
